c94618d681b32edeb3d1fbf31198d815bc948435
Ethan Favia <efavia215@live.com>
2016-10-12 15:49:33 -0400

    added main component and the package for it to webpack

--- app/app.jsx
+++ app/app.jsx
@@ -1,12 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Main = require('Main');
 
-
-
-
-
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 ReactDOM.render(
-        <h1> Boilerplate app!</h1>,
-                    document.getElementById('app') 
+        <Router history={hashHistory}> 
+            <Route path="/" component={Main}>
+
+            </Route> 
+        </Router>,
+        document.getElementById('app') 
                 )
