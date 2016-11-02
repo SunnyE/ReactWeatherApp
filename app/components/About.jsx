5657a5a3c93ba5fa28b315349e01db276c47ba4d
Ethan Favia <efavia215@live.com>
2016-11-02 15:13:47 -0400

    work on the about page with links and such

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,22 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h3> About Component </h3> 
+        <div>
+            <h1 className="text-center"> About</h1>
+            <p> This is a weather application built with React. I built it while doing a class on React.  </p>
+            <p>
+                Here are some of the resources that made this possible 
+            </p> 
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a>
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">Open Weather Map</a>
+                </li>
+            </ul>
+        </div>
+        
     )
 };
 
