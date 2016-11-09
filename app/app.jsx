e50a8e38137e4f3eeb17e30dc30e8248299e381c
Ethan Favia <efavia215@live.com>
2016-11-09 15:36:22 -0500

    started to scss styling

--- app/app.jsx
+++ app/app.jsx
@@ -13,7 +13,7 @@ require('style!css!foundation-sites/dist/foundation.min.css')
 
 $(document).foundation(); 
 
-require('style!css!applicationStyles')
+require('style!css!sass!applicationStyles')
 
 
 ReactDOM.render(
