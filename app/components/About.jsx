d3edec55691f49c2d9ab2799ef4d4f8042e1e9f9
Ethan Favia <efavia215@live.com>
2016-11-09 15:14:52 -0500

    more styling and cleaning

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h1 className="text-center"> About</h1>
+            <h1 className="text-center page-title"> About</h1>
             <p> This is a weather application built with React. I built it while doing a class on React.  </p>
             <p>
                 Here are some of the resources that made this possible 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -29,7 +29,7 @@ var Nav = React.createClass({
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
                             <li>
-                                <input type="Search"  placeholder="Search Weather"/>
+                                <input type="Search"  placeholder="Search Weather by city"/>
                             </li>
                             <li>
                                 <input type="submit"  className="button" value="Get Weather"/>
